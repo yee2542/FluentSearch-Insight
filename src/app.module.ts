2fe03dd66fec661f32eb6ccbe23fadd554c338ed
yee2542 <yee2542gtl@gmail.com>
2021-05-27 05:46:18 +0700

    add insight module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InsightModule } from './insight/insight.module';
 
 @Module({
-  imports: [],
+  imports: [InsightModule],
   controllers: [AppController],
   providers: [AppService],
 })
